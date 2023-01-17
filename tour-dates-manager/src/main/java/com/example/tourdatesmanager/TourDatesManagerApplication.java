55fa466bca2f75f8a5e09ff2d0439ca08a322180
Dominik Bilski <dominik.bilski24@gmail.com>
2023-01-17 21:48:40 +0100

    Initialize back end.

---
+++ tour-dates-manager/src/main/java/com/example/tourdatesmanager/TourDatesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourdatesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourDatesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourDatesManagerApplication.class, args);
+	}
+
+}
---
+++ tour-dates-manager/src/test/java/com/example/tourdatesmanager/TourDatesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourdatesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourDatesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
